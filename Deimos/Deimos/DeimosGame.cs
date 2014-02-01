171e6dd8fee16e2bf90dd1282d6afd067c844f98
Erenus <derman_e@epita.fr>
2014-02-01 15:05:07 +0100

    Manu est con

--- Deimos/Deimos/DeimosGame.cs
+++ Deimos/Deimos/DeimosGame.cs
@@ -99,7 +99,7 @@ namespace Deimos
                 this,
                 new Vector3(0f, 10f, 20f),
                 Vector3.Zero,
-                70f
+                30f
             );
             Components.Add(Camera);
 
@@ -118,11 +118,11 @@ namespace Deimos
             IsMouseVisible = false;
 
             // Game settings
-            //Graphics.PreferredBackBufferWidth = 1344;
-           // Graphics.PreferredBackBufferHeight = 840;
-            Graphics.PreferredBackBufferWidth = 1920;
-            Graphics.PreferredBackBufferHeight = 1080;
-            Graphics.IsFullScreen = true;
+            Graphics.PreferredBackBufferWidth = 1344;
+            Graphics.PreferredBackBufferHeight = 840;
+            //Graphics.PreferredBackBufferWidth = 1920;
+            //Graphics.PreferredBackBufferHeight = 1080;
+            Graphics.IsFullScreen = false;
             //graphics.PreferMultiSampling = true; // Anti aliasing - Useless as custom effects
             //graphics.SynchronizeWithVerticalRetrace = false; // VSync
             //IsFixedTimeStep = false; // Call the UPDATE method all the time instead of x time per sec
@@ -141,7 +141,7 @@ namespace Deimos
 
             ScreenElementManager = new ScreenElementManager(GraphicsDevice);
 
-            //DebugScreen = new DebugScreen(this);
+            DebugScreen = new DebugScreen(this);
         }
 
          

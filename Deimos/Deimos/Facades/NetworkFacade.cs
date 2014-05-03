c81e833dff9c153ef8daf0cb4123156637aceffa
Erenus <derman_e@epita.fr>
2014-05-03 16:05:00 +0200

    Minor: set initialmp state to inactive

--- Deimos/Deimos/Facades/NetworkFacade.cs
+++ Deimos/Deimos/Facades/NetworkFacade.cs
@@ -10,7 +10,7 @@ namespace Deimos.Facades
     static class NetworkFacade
     {
         // FOR DEVELOPMENT PURPOSES ONLY //
-        static public bool IsMultiplayer = true;
+        static public bool IsMultiplayer = false;
         ////////         /////          //////
 
         static public MainHandler MainHandling = new MainHandler();

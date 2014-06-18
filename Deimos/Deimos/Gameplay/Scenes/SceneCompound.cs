1496091154a9cf305d78a0932647239333f5f3aa
Erenus <erenus@mail.com>
2014-06-18 18:55:07 +0200

    Handling the ghost player problem

--- Deimos/Deimos/Gameplay/Scenes/SceneCompound.cs
+++ Deimos/Deimos/Gameplay/Scenes/SceneCompound.cs
@@ -58,6 +58,24 @@ namespace Deimos
                  0.2f
             );
 
+            if (NetworkFacade.IsMultiplayer)
+            {
+                foreach (KeyValuePair<byte, Player> p in NetworkFacade.Players)
+                {
+                    if (!GeneralFacade.SceneManager.ModelManager.LevelModelExists(p.Value.Name))
+                    {
+                        GeneralFacade.SceneManager.ModelManager.LoadModel(
+                            p.Value.Name,
+                            p.Value.GetModelName(),
+                            p.Value.Position,
+                            p.Value.Rotation,
+                            5,
+                            LevelModel.CollisionType.None
+                            );
+                    }
+                }
+            }
+
             token_rl = Objects.AddWeapon("BazookaPickup",
                 2,
                 new Vector3(17, -6, 110),
